edadb8425b0278e8c6ed47102c8950fcd9de9ecd
yalejop <alejo.9211@hotmail.com>
2022-01-29 23:27:43 -0500

    filter all inputs in the content and show in the HTML

--- js/app.js
+++ js/app.js
@@ -48,22 +48,33 @@ year.addEventListener('change', e => {
 
 minimo.addEventListener('change', e => {
     datosBusqueda.minimo = e.target.value;
+
+
+    filtrarAuto();
 })
 
 maximo.addEventListener('change', e => {
     datosBusqueda.maximo = e.target.value;
+
+    filtrarAuto();
 })
 
 puertas.addEventListener('change', e => {
-    datosBusqueda.puertas = e.target.value;
+    datosBusqueda.puertas = parseInt(e.target.value);
+
+    filtrarAuto();
 })
 
 transmision.addEventListener('change', e => {
     datosBusqueda.transmision = e.target.value;
+
+    filtrarAuto();
 })
 
 color.addEventListener('change', e => {
     datosBusqueda.color = e.target.value;
+
+    filtrarAuto();
 })
 
 
@@ -105,14 +116,14 @@ function llenarSelect() {
 }
 
 function filtrarAuto() {
-    const resultado = autos.filter(filtrarMarca).filter(filtrarYear)
+    const resultado = autos.filter(filtrarMarca).filter(filtrarYear).filter(filtrarMinimo).filter(filtrarMaximo).filter(filtrarPuertas).filter(filtrarTransmision).filter(filtrarColor)
 
     mostrarAutos(resultado)
-    //console.log(resultado)
+    console.log(resultado)
 }
 
 function filtrarMarca(auto) {
-    const { marca } = datosBusqueda
+    const { marca } = datosBusqueda;
     if (marca) {
         return auto.marca === marca;
     }
@@ -120,9 +131,49 @@ function filtrarMarca(auto) {
 }
 
 function filtrarYear(auto) {
-    const { year } = datosBusqueda
+    const { year } = datosBusqueda;
     if (year) {
         return auto.year === year;
+    }
+    return auto;
+}
+
+function filtrarMinimo(auto) {
+    const { minimo } = datosBusqueda;
+    if (minimo) {
+        return auto.precio >= minimo;
+    }
+    return auto;
+}
+
+function filtrarMaximo(auto) {
+    const { maximo } = datosBusqueda;
+    if (maximo) {
+        return auto.precio <= maximo;
+    }
+    return auto;
+}
+
+function filtrarPuertas(auto) {
+    const { puertas } = datosBusqueda;
+    if (puertas) {
+        return auto.puertas === puertas;
+    }
+    return auto;
+}
+
+function filtrarTransmision(auto) {
+    const { transmision } = datosBusqueda;
+    if (transmision) {
+        return auto.transmision === transmision;
+    }
+    return auto;
+}
+
+function filtrarColor(auto) {
+    const { color } = datosBusqueda;
+    if (color) {
+        return auto.color === color;
     }
     return auto;
 }
